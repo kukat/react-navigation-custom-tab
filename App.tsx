2769f1734c3862314a47910d9fbc43527adb01e6
Cheng Yao <kukat@me.com>
2024-05-09 03:45:14 +0800

    customized tabbar

--- App.tsx
+++ App.tsx
@@ -5,113 +5,96 @@
  * @format
  */
 
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {MyTabBar} from './MyTabBar';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function HomeScreen({navigation}) {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.view}>
+      <Text>Home!</Text>
+      <Button
+        title="Go to Explore"
+        onPress={() => {
+          navigation.navigate('Popup');
+        }}
+      />
     </View>
   );
 }
+function ExploreScreen() {
+  return (
+    <View style={styles.view}>
+      <Text>Explore!</Text>
+    </View>
+  );
+}
+function ScanScreen() {
+  return (
+    <View style={styles.view}>
+      <Text>Scan!</Text>
+    </View>
+  );
+}
+function RewardsScreen() {
+  return (
+    <View style={styles.view}>
+      <Text>Rewards!</Text>
+    </View>
+  );
+}
+function ProfileScreen() {
+  return (
+    <View style={styles.view}>
+      <Text>Profile!</Text>
+    </View>
+  );
+}
+const PopupScreen = () => {
+  return (
+    // eslint-disable-next-line react-native/no-inline-styles
+    <View style={[styles.view, {backgroundColor: 'lightblue'}]}>
+      <Text>Popup!</Text>
+    </View>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator tabBar={props => <MyTabBar {...props} />}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Explore" component={ExploreScreen} options={{tabBarBadge: 3}} />
+      <Tab.Screen name="Scan" component={ScanScreen} />
+      <Tab.Screen name="Rewards" component={RewardsScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Tab" component={TabNavigator} options={{headerShown: false}} />
+        <Stack.Screen name="Popup" component={PopupScreen} options={{presentation: 'modal'}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#673ab7',
   },
 });
 
---
+++ MyTabBar.tsx
@@ -0,0 +1,73 @@
+import {BottomTabBarProps} from '@react-navigation/bottom-tabs';
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+export const MyTabBar = ({state, descriptors, navigation}: BottomTabBarProps) => {
+  return (
+    <View style={styles.container}>
+      {state.routes.map((route, index) => {
+        const {options} = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            if (route.name === 'Scan') {
+              navigation.navigate('Popup');
+              return;
+            }
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? {selected: true} : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={styles.touchableContainer}>
+            <View style={[styles.tab, isFocused && styles.tabFocused]}>
+              {typeof label === 'string' ? <Text>{label}</Text> : <label />}
+            </View>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {flexDirection: 'row'},
+  touchableContainer: {flex: 1},
+  tab: {
+    backgroundColor: '#222',
+    height: 44,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tabFocused: {
+    backgroundColor: '#673ab7',
+  },
+});
